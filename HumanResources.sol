9e997146f2c3cc6e21490d595a9ec0fe9bb9c73d
Yazan Ayyoub <yazayyoub@gmail.com>
2024-11-27 13:35:45 +0000

    completed terminated and registration

--- HumanResources.sol
+++ HumanResources.sol
@@ -6,15 +6,20 @@ import "./IHumanResources.sol";
 
 abstract contract HumanResources is IHumanResources {
 
+    enum preferredPay {USDC, ETH}
+
     struct Employee{
         uint256 weeklyUsdSalary;
         uint256 employedSince;
         uint256 terminatedAt;
+        uint256 accumulated;
+        preferredPay preference;
     }
 
     address public immutable hrManagerAddress; 
     mapping(address => Employee) public employeeMap;
     address[] public employeeAddresses;
+    uint256 employeeCount;
 
     constructor(){
         hrManagerAddress = msg.sender; 
@@ -35,14 +40,7 @@ abstract contract HumanResources is IHumanResources {
     }
 
     function getActiveEmployeeCount() external view returns(uint256){
-        uint256 count = 0;
-        for(uint256 i; i < employeeAddresses.length; i++){
-            Employee memory currentEmployee = employeeMap[employeeAddresses[i]];
-            if(currentEmployee.terminatedAt == 0){
-                count++;
-            }
-        }
-        return count;
+        return employeeCount;
     }
 
     function getEmployeeInfo(address employee) external view returns(uint256, uint256, uint256){
@@ -51,23 +49,38 @@ abstract contract HumanResources is IHumanResources {
     } 
 
     // set to 18 decimal places
-    // read spec!
+    // read spec! should i have an addresses array.
     // complete implementation, check if accumulated salary should continue or not
-    function registerEmployee (address employee, uint256 weeklyUsdSalary) external {
-        uint256 checkEmployee = employeeMap[employee].weeklyUsdSalary;
+    function registerEmployee (address employee, uint256 weeklyUsdSalary) onlyHrManager external {
+        uint256 checkEmployee = employeeMap[employee].employedSince;
+        uint256 checkEmployee2 = employeeMap[employee].terminatedAt;
         if (checkEmployee == 0){
-            Employee memory newEmployee = Employee(weeklyUsdSalary, block.timestamp, 0);
+            Employee memory newEmployee = Employee(weeklyUsdSalary * 10**18, block.timestamp, 0, 0, preferredPay.USDC);
             employeeMap[employee] = newEmployee;
             employeeAddresses.push(employee);
-
+            employeeCount += 1;
             emit EmployeeRegistered(employee, weeklyUsdSalary);
         }
-        else {
+        else if (checkEmployee2 == 0){
             revert EmployeeAlreadyRegistered();
+        }
+        else {
+            employeeMap[employee].weeklyUsdSalary = weeklyUsdSalary * 10**18;
+            employeeMap[employee].employedSince = block.timestamp;
+            employeeMap[employee].terminatedAt = 0;
         }
     }
 
+    function terminateEmployee (address employee) onlyHrManager external {
+        if(employeeMap[employee].employedSince  == 0){
+            revert EmployeeNotRegistered();
+        }
+        employeeMap[employee].terminatedAt = block.timestamp;
+        emit EmployeeTerminated(employee);
+    }
 
+    function 
+    
 
     
 }
